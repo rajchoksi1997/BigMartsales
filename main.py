cd4dcc1e62bc270a9d824aaf1d4f6797e28ccd99
rajchoksi1997 <rajchoksi1997@gmail.com>
2020-09-09 23:18:22 +0530

    sales data prediction project

--- main.py
+++ main.py
@@ -6,33 +6,37 @@ from flask_cors import CORS, cross_origin
 from prediction_Validation_Insertion import pred_validation
 from trainingModel import trainModel
 from training_Validation_Insertion import train_validation
-#import flask_monitoringdashboard as dashboard
+# import flask_monitoringdashboard as dashboard
 from predictFromModel import prediction
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
-#dashboard.bind(app)
+# dashboard.bind(app)
 CORS(app)
+
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
 
+
 @app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRouteClient():
+    print("****")
+    print(request)
     try:
         if request.json['folderPath'] is not None:
             path = request.json['folderPath']
 
-            pred_val = pred_validation(path) #object initialization
+            # pred_val = pred_validation(path) #object initialization
 
-            pred_val.prediction_validation() #calling the prediction_validation function
+            #   pred_val.prediction_validation() #calling the prediction_validation function
 
-            pred = prediction(path) #object initialization
+            pred = prediction(path)  # object initialization
 
             # predicting for dataset present in database
             path = pred.predictionFromModel()
@@ -40,38 +44,35 @@ def predictRouteClient():
         elif request.form is not None:
             path = request.form['folderPath']
 
-            pred_val = pred_validation(path) #object initialization
+            pred_val = pred_validation(path)  # object initialization
 
-            pred_val.prediction_validation() #calling the prediction_validation function
+            pred_val.prediction_validation()  # calling the prediction_validation function
 
-            pred = prediction(path) #object initialization
+            pred = prediction(path)  # object initialization
 
             # predicting for dataset present in database
             path = pred.predictionFromModel()
             return Response("Prediction File created at %s!!!" % path)
     except ValueError:
-        return Response("Error Occurred! %s" %ValueError)
+        return Response("Error Occurred! %s" % ValueError)
     except KeyError:
-        return Response("Error Occurred! %s" %KeyError)
+        return Response("Error Occurred! %s" % KeyError)
     except Exception as e:
-        return Response("Error Occurred! %s" %e)
-
+        return Response("Error Occurred! %s" % e)
 
 
 @app.route("/train", methods=['POST'])
 @cross_origin()
 def trainRouteClient():
-
     try:
         if request.json['folderPath'] is not None:
             path = request.json['folderPath']
-            train_valObj = train_validation(path) #object initialization
+            train_valObj = train_validation(path)  # object initialization
 
-            train_valObj.train_validation()#calling the training_validation function
+            train_valObj.train_validation()  # calling the training_validation function
 
-
-            trainModelObj = trainModel() #object initialization
-            trainModelObj.trainingModel() #training the model for the files in the table
+            trainModelObj = trainModel()  # object initialization
+            trainModelObj.trainingModel()  # training the model for the files in the table
 
 
     except ValueError:
@@ -87,11 +88,12 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successfull!!")
 
-#port = int(os.getenv("PORT"))
+
+# port = int(os.getenv("PORT"))
 if __name__ == "__main__":
-  #  host = '0.0.0.0'
-  #  port = 5000
-  #  httpd = simple_server.make_server(host, port, app)
-  #  print("Serving on %s %d" % (host, port))
-  #  httpd.serve_forever()
-  app.run(debug=True)
+    #  host = '0.0.0.0'
+    #  port = 5000
+    #  httpd = simple_server.make_server(host, port, app)
+    #  print("Serving on %s %d" % (host, port))
+    #  httpd.serve_forever()
+    app.run(debug=True, port=5001)
